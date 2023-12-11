de9dac1d8cd4508c4c8ef14916bcf3ba8804aa05
farhan <mohdfarhanpt@outlook.com>
2023-12-11 20:09:11 +0530

    add new origin

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
+// ***React.createElement => ReactElement- JS Object => HTMLElement(render)
+
 // this empty object  {} is the place where you give attribute to your tag
 const heading = React.createElement(   //creating a javasript Object or react element
 'h1',                                             //fist argument - what element you want to create
 {id: "heading",xyz: "abc"},  //attributes props   //second argument - give attributes
 "Hello world😎")   //child props                    //third argument - children of the first argument
 
+
+// JSX (transpiled before it reaches JS engine) - PARCEL - Babel
+
+// ****JSX => React.createElement => ReactElement- JS Object => HTMLElement(render)
+// BABEL converts JSX => React.createElement
+const jsxHeading = <img src="" />
+// its hard to read not user friendly but this is core of react element
+// then JSX created - js syntax esier to create react elements
+
 console.log(heading)  // Object
+console.log(jsxHeading);
 // this heading  is react element(object)
 // react element is nothing but normal javascript object
 
@@ -18,7 +30,7 @@ console.log(heading)  // Object
 
 
 const root = ReactDOM.createRoot(document.getElementById("root"))
-root.render(heading)
+root.render(jsxHeading)
 
 // React.createElement => Object => HTMLElement(rendered)
 
@@ -44,33 +56,28 @@ root.render(heading)
 */
 
 
-const parent = React.createElement(
-"div",
-{id: "parent"},
-[
-    React.createElement(
-        "div",
-        {id: "child"},
-        [
-            React.createElement("h1",{},"I am h1 tag"),
-            React.createElement("h2",{},"I am h2 tag")
-        ]
-        // use array to create siblings
-    ),
-    React.createElement(
-        "div",
-        {id: "child"},
-        [
-            React.createElement("h1",{},"I am h1 tag"),
-            React.createElement("h2",{},"I am h2 tag")
-        ]
-        // use array to create siblings
-    )
-]
-);
-const root2 = ReactDOM.createRoot(document.getElementById("root2"))
-root2.render(parent)
-
-
-// using react.createElement code become so messy not readable
-// JSX(javascript systax extension)
+// const parent = React.createElement(
+// "div",
+// {id: "parent"},
+// [
+//     React.createElement(
+//         "div",
+//         {id: "child"},
+//         [
+//             React.createElement("h1",{},"I am h1 tag"),
+//             React.createElement("h2",{},"I am h2 tag")
+//         ]
+//     ),
+//     React.createElement(
+//         "div",
+//         {id: "child"},
+//         [
+//             React.createElement("h1",{},"I am h1 tag"),
+//             React.createElement("h2",{},"I am h2 tag")
+//         ]
+//         )
+//     ]
+//     );
+//     const root2 = ReactDOM.createRoot(document.getElementById("root2"))
+//     root2.render(parent)
+    // use array to create siblings
--- notes/e3p3-jsx.md
+++ notes/e3p3-jsx.md
@@ -1,2 +1,31 @@
 # e3p3-jsx
+- JSX - js syntax esier to create react elements
+- Jsx is not part of react (fudamental diff) react also write without jsx
+- jsx is quite helpful
 
+- Core of react
+- const heading = React.createElement(   //creating a javasript Object or react element
+  'h1',                                             //fist argument - what element you want to create
+  {id: "heading",xyz: "abc"},  //attributes props   //second argument - give attributes
+  "Hello world")
+
+- <h1>Hello world</h1> react element
+- jsx is diff from html , jsx is html like syntax, jsx is not html written in js,xml like syntax
+- JSX
+- ****JSX => React.createElement => ReactElement- JS Object =>       HTMLElement(render)
+// BABEL converts JSX => React.createElement
+- const jsxHeading = <h1>Hello world</h1>
+- js engine of browser does not understand above piece of code , js engine understand ecma scripts then how this code working PARCEL(babel) is doing the job behind the scene , the above code is transpiled before goes to js engine
+- parcel has given this transpilation job to BABEL
+- BABEL - is a js compiler or package  , parcel install babel(node_modules)
+- it taked yout jsx and converts into the code that js engine understands
+
+- JSX (transpiled before it reaches JS engine) - PARCEL - Babel
+- attributes are in camel casing
+- hw - try to use diff att inside jsx
+- if write jsx is in single line - perfectly valid jsx but if write in
+multiple lines it needs to wrap around parenthesis because babel needs to understand where it starts and ends
+
+# Babel
+- babel also transipled code es-6 code that older browser understand
+- read babel
