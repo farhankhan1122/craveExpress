bcc1423fea006ef85ee736e990c81a1b5abf0660
Farhan Khan <kfarhan634@MacBook-Air.local>
2025-03-29 17:58:35 +0530

    update: header switch

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,4 @@
-import React, { useState, useContext } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { LOGO_URL } from "../utils.js/constants"; //because named export
 import { Link } from "react-router-dom";
 // import { ReactDOM } from "react-dom/client";
@@ -20,16 +20,33 @@ export const Header = ({ activeHeader }) => {
   const handleClearItem = () => {
     dispatch(clearItems());
   };
-  console.log(cartItems, "cartItems");
+
+  const [isScrolled, setIsScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setIsScrolled(true);
+      } else {
+        setIsScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
 
   return (
     <div
-      className={
-        activeHeader
-          ? "fixed z-[10] w-full flex items-center justify-between h-[80px] px-[20px] text-[18px] text-white"
-          : "fixed z-[5] w-full flex items-center justify-between h-[80px] px-[20px] text-[18px] text-black bg-white shadow-custom"
-      }
-    >
+    className={`fixed w-full flex items-center justify-between h-[80px] px-[20px] text-[18px] 
+      ${activeHeader ? "z-[10]" : "z-[5]"} 
+      ${activeHeader && !isScrolled ? "text-white" : "text-black"} 
+      ${isScrolled ? "bg-white shadow-custom" : "bg-transparent"}`}
+  >
+        
       <div className="w-[120px] h-auto relative">
         {/* <img className="rounded-full" slt="food logo" src="/public/icons/BiteLogo.svg" /> */}
         <Link to="/">
@@ -52,10 +69,21 @@ export const Header = ({ activeHeader }) => {
             <Link to={"/grocery"}>Grocery store</Link>
           </li>
           <li className="relative">
-            <Link className="flex items-center gap-2 cart_item_wrapper" to={"/cart"}>
-            <span class="cart_el cart_item">
-              <svg class="cart_svg cart_svg_color" viewBox="-1 0 37 32" height="20" width="20" fill="#686b78"><path d="M4.438 0l-2.598 5.11-1.84 26.124h34.909l-1.906-26.124-2.597-5.11z"></path></svg>
-              <span class="cart_item_count">{cartItems.length}</span>
+            <Link
+              className="flex items-center gap-2 cart_item_wrapper"
+              to={"/cart"}
+            >
+              <span class="cart_el cart_item">
+                <svg
+                  class="cart_svg cart_svg_color"
+                  viewBox="-1 0 37 32"
+                  height="20"
+                  width="20"
+                  fill="#686b78"
+                >
+                  <path d="M4.438 0l-2.598 5.11-1.84 26.124h34.909l-1.906-26.124-2.597-5.11z"></path>
+                </svg>
+                <span class="cart_item_count">{cartItems.length}</span>
               </span>
               <span>Cart</span>
             </Link>
--- src/components/Shimmer.js
+++ src/components/Shimmer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Shimmer = () => {
   return <div className="">
-    <div className="h-[65dvh] w-full shimmer_card"></div>
+    <div className="h-[65dvh] w-full shimmer_card_video"></div>
     <div className="shimmer_container res-container">
       <div className="res-card shimmer_card"></div>
       <div className="res-card shimmer_card"></div>
